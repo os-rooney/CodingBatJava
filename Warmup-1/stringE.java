e2085d1752794eb3e8621e7bb5a73119cc2e59f7
Rooney H. Osman <dev@ronios.de>
2022-03-09 20:46:07 +0100

    upload stringE

---
+++ Warmup-1/stringE.java
@@ -0,0 +1,9 @@
+/**
+ * https://codingbat.com/prob/p173784
+ * Return true if the given string contains between 1 and 3 'e' chars.
+ */
+public class stringE {
+    public static boolean stringE(String str){
+
+    }
+}
