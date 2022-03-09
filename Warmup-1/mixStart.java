d5b4bd565d68fb1969aca6bb9478b2167c5594ee
Rooney H. Osman <dev@ronios.de>
2022-03-09 18:24:15 +0100

    upload mixStart

---
+++ Warmup-1/mixStart.java
@@ -0,0 +1,17 @@
+/**
+ * Source: CodingBat, https://codingbat.com/prob/p151713
+ * Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix".. all count. 
+ */
+public class mixStart {
+    public static boolean mixStart(String str){
+        if(str.charAt(1) == 'i' && str.charAt(2) == 'x'){
+            return true;
+        }
+        return false;
+    }
+   public static void main(String[] args) {
+       System.out.println(mixStart("mix snacks"));
+       System.out.println(mixStart("pix snacks"));
+       System.out.println(mixStart("piz snacks"));
+   } 
+}
