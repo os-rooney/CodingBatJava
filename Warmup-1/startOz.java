75810e1435464760dc6fe354be19d0e47e633fec
Rooney H. Osman <dev@ronios.de>
2022-03-09 18:37:05 +0100

    upload startOz

---
+++ Warmup-1/startOz.java
@@ -0,0 +1,33 @@
+/**
+ * Source: CodingBat, https://codingbat.com/prob/p199720
+ * Given a string, return as string made of the first 2 chars (if present), 
+ * however include first char only if it is 'o' 
+ * and include the seconde only if it is 'z', so "ozymandias" yields "oz".
+ */
+public class startOz {
+    public static String startOz(String str){
+        if(str.equals("")){
+            return "";
+          } else if(str.equals("o")){
+            return "o";
+          } else if(str.length() >= 2){
+              if(str.charAt(0)=='o' && str.charAt(1)== 'z'){
+              return "oz";
+            } else if(str.charAt(0) != 'o' && str.charAt(1) == 'z'){
+              return "z";
+            } else if(str.charAt(0) == 'o' && str.charAt(1) != 'z'){
+              return "o";
+            }
+          }
+          return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(startOz("ozymandias"));
+        System.out.println(startOz("o"));
+        System.out.println(startOz("oz"));
+        System.out.println(startOz(""));
+        System.out.println(startOz("abc"));
+        System.out.println(startOz("aztec"));
+    }
+}
