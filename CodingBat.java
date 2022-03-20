b3923dabc26cc63b9479a215a1641679773386a2
Rooney H. Osman <dev@ronios.de>
2022-03-20 22:04:25 +0100

    add array123 from Warmup-2

--- CodingBat.java
+++ CodingBat.java
@@ -559,4 +559,17 @@ public class CodingBat {
         return count;
     }
 
+
+    /**
+     * Given an array of ints, return true if the sequence of numbers 1,2,3 appears in the array somewhere.
+     * @param nums array of ints
+     * @return boolean true if the sequence of numbers 1, 2, 3 appears
+     */
+    public static boolean array123(int[] nums){
+        for(int i = 0; i < nums.length-2; i++){
+            if(nums[i] == 1 && nums[i+1] == 2 && nums[i+2] == 3) return true;
+          }
+          return false;
+    }
+
 }
